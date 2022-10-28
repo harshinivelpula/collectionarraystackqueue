2a63d02db6e63a45bf0c3641ab63b3edae202ef0
DELL <105187636+harshinivelpula@users.noreply.github.com>
2022-10-28 19:17:52 +0530

    list

---
+++ CollectionListDemo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CollectionDemo.cs
+{
+    internal class CollectionListDemo
+    {
+        public void Display()
+        {
+            List<string> list = new List<string>();
+
+            list.Add("ramesh");
+            list.Add("suresh");
+            list.Add("rita");
+
+            foreach(string ele in list)
+            {
+                Console.WriteLine(ele);
+            }
+        }
+    }
+}
