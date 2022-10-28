0827564b87596719768006f76e874f25f0aa5909
DELL <105187636+harshinivelpula@users.noreply.github.com>
2022-10-28 19:16:34 +0530

    adding program

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using CollectionDemo.cs;
+
+namespace CollectionDemo
+{
+    class program
+    {
+        public static void Main(string[] args)
+        {
+            ArrayListDemo arrayListDemo = new ArrayListDemo();
+            arrayListDemo.Display();
+
+
+
+            CollectionListDemo collectionListDemo = new CollectionListDemo();
+            collectionListDemo.Display();
+
+
+            CollectionStackDemo collectionStackDemo = new CollectionStackDemo();
+            collectionStackDemo.Display();
+
+
+            CollectionQueueDemo collectionQueueDemo = new CollectionQueueDemo();
+            collectionQueueDemo.Display();
+        }
+    }
+
+  
+}
+
