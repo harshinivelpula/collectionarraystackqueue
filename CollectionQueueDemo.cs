3ea8ba6c64e682e8af9fbd38aa5042fb10c4ba89
DELL <105187636+harshinivelpula@users.noreply.github.com>
2022-10-28 19:19:00 +0530

    queue

---
+++ CollectionQueueDemo.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CollectionDemo.cs
+{
+    public class CollectionQueueDemo
+    {
+        public void Display()
+        {
+            Queue<int> queu = new Queue<int>();
+
+            queu.Enqueue(1);
+            queu.Enqueue(2);
+            queu.Enqueue(3);
+            queu.Enqueue(4);
+
+            foreach(var item in queu)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("------");
+            Console.WriteLine("the peek element is " + queu.Peek());
+            Console.WriteLine("-------");
+            queu.Dequeue();
+            Console.WriteLine("after Dequeued");
+
+            foreach (int item in queu)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("------");
+            Console.WriteLine("the peek element is " + queu.Peek());
+            Console.WriteLine("-------");
+            queu.Dequeue();
+            Console.WriteLine("after Dequeued");
+
+            foreach (int item in queu)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("------");
+            Console.WriteLine("the peek element is " + queu.Peek());
+            Console.WriteLine("-------");
+            queu.Dequeue();
+            Console.WriteLine("after Dequeued");
+
+            foreach (int item in queu)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+    }
+}
