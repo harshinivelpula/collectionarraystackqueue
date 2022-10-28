f9654b606d6b720f5939df5427b921e17b58ab32
DELL <105187636+harshinivelpula@users.noreply.github.com>
2022-10-28 19:18:28 +0530

    stack

---
+++ CollectionStackDemo.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CollectionDemo.cs
+{
+    public class CollectionStackDemo
+    {
+        public void Display()
+        {
+            Stack<int> stack = new Stack<int>();
+            stack.Push(22);
+            stack.Push(33);
+            stack.Push(45);
+            stack.Push(13);
+            stack.Push(24);
+            stack.Push(12);
+
+            foreach(int item in stack)
+            {
+                Console.WriteLine(item);
+            }
+
+            stack.Pop();
+
+            Console.WriteLine("------");
+            Console.WriteLine("the peek element is " + stack.Peek());
+            Console.WriteLine("after poped");
+
+            foreach (int item in stack)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("the peek element is " + stack.Peek());
+        }
+    }
+}
